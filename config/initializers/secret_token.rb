bfa5cbaa3aa2780378f347b475e778f929d32f26
saasbook <saasbook@saasbook.(none)>
2014-03-29 10:46:52 -0700

    Initialize blog application
    Signed-off-by: Shuo Jia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '7402926e13dfe0d3fefb2cd582eeddf6147880d766c10a925dcedd0cae33e6e725dc046e6d82fbd955643e5dae0ba1001d6e7781addeb8a5973e83203860a533'
